2c3e1456947bf426aa175463f95f3330b6d021df
Ani <ani-alaverdyan@mail.ru>
2022-09-23 09:41:25 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        List<Integer> nums = new ArrayList<>(List.of(16, 1, 8, 6, 22, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        List<Integer> nums = new ArrayList<>(List.of(16, 1, 8, 6, 22, 1, 2, 3, 4, 4, 5, 5, 6, 7,8));
         for (Integer n : nums) {
             if (n % 2 != 0) {
-                System.out.print(n + ", ");
+                System.out.print(n+ ", ");
             }
         }
         System.out.println();
@@ -30,19 +30,18 @@ public class Main {
 
         for (int i = 0; i < cities.size(); i++) {
             String c = cities.get(i);
-            if (!mapCities.containsKey(c)) {
-                mapCities.put(c, 1);
-            } else {
+            if (mapCities.containsKey(c)) {
                 mapCities.put(c, mapCities.get(c)+1);
+            } else {
+                mapCities.put(c, 1);
             }
         }
-
         for (Map.Entry<String, Integer> entry : mapCities.entrySet()) {
             if (entry.getValue() > 1) {
-                System.out.println("Город = "+entry.getKey()+ ", Повторений = " + entry.getValue());
+                System.out.print("Город-"+entry.getKey()+" Повторений-"+entry.getValue()+" ");
             }
         }
-
+        System.out.println();
         HashSet<String> citiesNoRepeats = new HashSet<>();
         HashSet<String> citiesRepeats = new HashSet<>();
         for (int i = 0; i < cities.size(); i++) {
@@ -53,7 +52,7 @@ public class Main {
                 citiesNoRepeats.add(c);
             }
         }
-        System.out.println("Повторения-"+citiesRepeats);
+        System.out.println("Города без повторов "+citiesNoRepeats);
     }
 }
 
